aabc8ebcfb13f44498358a59ae3ca64c82843e44
iwwwish <siramshettyv2@nih.gov>
2020-03-30 01:11:42 -0400

    external model predictions

--- ext_models/external_predictions.py
+++ ext_models/external_predictions.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 # load prediction file
-df = pd.read_csv('predictions_startdrop.csv', delimiter=',')
+df = pd.read_csv('validation_predherg.csv', delimiter=',')
 y_test = np.array(df['activity']).ravel()
 y_pred = np.array(df['prediction']).ravel()
 
@@ -13,8 +13,10 @@ import evaluation_metrics as ev
 
 ba = ev.balanced_accuracy(y_test, y_pred)
 sens, spec = ev.sensitivity_specificity(y_test, y_pred)
+kappa = ev.kappa_score(y_test, y_pred)
 
-print('\nModel Performance')
+print('Model Performance')
 print('BACC:\t%s' % ba)
 print('Sens:\t%s' % sens)
 print('Spec:\t%s' % spec)
+#print('Kappa:\t%s' % kappa)
