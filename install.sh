f3f909f9ba037b142e8fbb04501f9ad0ed47cfe6
iwwwish <siramshettyv2@nih.gov>
2020-12-14 03:18:37 -0500

    python packages for conda env

---
+++ install.sh
@@ -0,0 +1,10 @@
+pip install numpy==1.18.1
+pip install pandas==1.0.3
+pip install pickle5
+pip install keras==2.3.1
+pip install keras_self_attention==0.41.0
+pip install tensorflow==2.1.0
+pip install notebook
+pip install scikit-learn==0.23.1
+pip install xgboost==1.0.2
+pip install h5py==2.10.0
